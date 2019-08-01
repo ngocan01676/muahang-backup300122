e1197d9165460f086936922cdfe1f9c4349dedc3
SoftVN88 <softvn88@gmail.com>
2019-08-01 10:23:23 +0700

    a

--- zoe/Application.php
+++ zoe/Application.php
@@ -5,6 +5,10 @@ class Application extends App{
     public function __construct(?string $basePath = null)
     {
         parent::__construct($basePath);
-        $this->alias('files',\Zoe\Core\Filesystem::class);
+      //  $this->alias('files',\Zoe\Core\Filesystem::class);
+        $this->app->bind(\Illuminate\Filesystem\Filesystem::class, function()
+        {
+            return new \Zoe\Core\Filesystem();
+        });
     }
 }
