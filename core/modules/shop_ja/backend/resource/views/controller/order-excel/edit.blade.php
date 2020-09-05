f48b2596951bd597faaa01f3c95b0d5976dd8611
SoftVN88 <softvn88@gmail.com>
2020-09-05 12:16:43 +0700

    t

--- core/modules/shop_ja/backend/resource/views/controller/order-excel/edit.blade.php
+++ core/modules/shop_ja/backend/resource/views/controller/order-excel/edit.blade.php
@@ -1,7 +1,7 @@
 @section('content-header')
     <h1>
         {!! @z_language(["Chức năng quản lý đơn hàng Excel"]) !!}
-        <button type="button" class="btn btn-default btn-md btnSave"> Save </button>
+         
     </h1>
 @endsection
 @section('content')
