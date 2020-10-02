d67a45c5c1f9bdee7c48b79ad53eadc8c78d2aeb
SoftVN88 <softvn88@gmail.com>
2020-10-02 17:28:35 +0700

    t

--- core/modules/shop_ja/backend/src/Http/Controllers/OrderExcelController.php
+++ core/modules/shop_ja/backend/src/Http/Controllers/OrderExcelController.php
@@ -386,7 +386,7 @@ class OrderExcelController extends \Zoe\Http\ControllerBackend
                         }catch (\Exception $ex){
                             $datas = ['error'=>$ex->getMessage()];
                         }
-                        
+
                     }else{
                         try{
                             foreach ($order['data'] as $key=>$values){
@@ -503,7 +503,7 @@ class OrderExcelController extends \Zoe\Http\ControllerBackend
                         }
                     }
                     $this->log('shop_js:orderExcel',$type,['id' => $model->id]);
-                    return response()->json(['id'=>$model->id,'url1'=>route('backend:shop_ja:order:excel:edit', ['id' => $model->id]),'logs'=>$logs]);
+                    return response()->json(['id'=>$model->id,'url'=>route('backend:shop_ja:order:excel:edit', ['id' => $model->id]),'logs'=>$logs]);
                 }
                 else
                     return response()->json($datas);
