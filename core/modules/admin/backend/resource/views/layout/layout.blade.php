8a13496a2724d69fdee101c0011c630f98f3cc70
SoftVN88 <softvn88@gmail.com>
2020-10-22 14:19:06 +0700

    t

--- core/modules/admin/backend/resource/views/layout/layout.blade.php
+++ core/modules/admin/backend/resource/views/layout/layout.blade.php
@@ -229,7 +229,7 @@
             @foreach($rs as $val)
             <div class="alert alert-success alert-dismissible">
                 <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                <h4><i class="icon fa fa-check"></i> {!! $val->title !!}!</h4>
+                <h4><i class="icon fa fa-check"></i> {!! $val->title !!}! {!! $val->updated_at !!}</h4>
                 {!! $val->message !!}
             </div>
             @endforeach
