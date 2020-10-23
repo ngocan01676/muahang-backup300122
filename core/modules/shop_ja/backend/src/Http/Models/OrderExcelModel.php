84ba15f0473b07beebd8f3febbb69f5f42934a98
SoftVN88 <softvn88@gmail.com>
2020-10-23 15:12:44 +0700

    t

--- core/modules/shop_ja/backend/src/Http/Models/OrderExcelModel.php
+++ core/modules/shop_ja/backend/src/Http/Models/OrderExcelModel.php
@@ -19,11 +19,11 @@ class OrderExcelModel extends Model
             ->where('status',1)
             ->where('created_at','>=',$date." 00:00:00")
             ->where('created_at','<=',$date." 23:59:59")
-            ->orderBy('pay_method','asc')
+
             ->get()->all();
         $datas = [];
         foreach ($lists as $key=>$value){
-           $shop_order_excel =  DB::table('shop_order_excel')->where('session_id',$value->id)->get()->all();
+           $shop_order_excel =  DB::table('shop_order_excel')->where('session_id',$value->id)->orderBy('pay_method','asc')->get()->all();
            foreach ($shop_order_excel as $_key=>$_value){
                $datas[] = $_value;
            }
