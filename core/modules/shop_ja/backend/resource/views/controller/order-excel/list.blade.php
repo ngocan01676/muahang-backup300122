65d570d98a3427c919a0aa475f1499dd9477df38
SoftVN88 <softvn88@gmail.com>
2020-10-23 17:28:02 +0700

    t

--- core/modules/shop_ja/backend/resource/views/controller/order-excel/list.blade.php
+++ core/modules/shop_ja/backend/resource/views/controller/order-excel/list.blade.php
@@ -18,111 +18,111 @@
     @breadcrumb()@endbreadcrumb
     @component('backend::layout.component.list',['name'=>'module:shop_ja:order:excel','models'=>$models,'callback'=>$callback])
         @slot("tool")
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="form-group">
-                        <label>Thời gian:</label>
-                        <div class="input-group">
-                            <div class="input-group-addon">
-                                <i class="fa fa-calendar"></i>
-                            </div>
-                            <input type="text" class="form-control pull-right" id="reservation">
-                        </div>
-                        <!-- /.input group -->
-                    </div>
-                </div>
-            </div>
-            <div class="result-analytics">
-                <div class="row">
-                    <div class="col-md-2 col-sm-6 col-xs-12">
-                        <div class="info-box">
-                            <span class="info-box-icon bg-aqua"><i class="ion ion-ios-cart-outline"></i></span>
+            {{--<div class="row">--}}
+                {{--<div class="col-md-3">--}}
+                    {{--<div class="form-group">--}}
+                        {{--<label>Thời gian:</label>--}}
+                        {{--<div class="input-group">--}}
+                            {{--<div class="input-group-addon">--}}
+                                {{--<i class="fa fa-calendar"></i>--}}
+                            {{--</div>--}}
+                            {{--<input type="text" class="form-control pull-right" id="reservation">--}}
+                        {{--</div>--}}
+                        {{--<!-- /.input group -->--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="result-analytics">--}}
+                {{--<div class="row">--}}
+                    {{--<div class="col-md-2 col-sm-6 col-xs-12">--}}
+                        {{--<div class="info-box">--}}
+                            {{--<span class="info-box-icon bg-aqua"><i class="ion ion-ios-cart-outline"></i></span>--}}
 
-                            <div class="info-box-content">
-                                <span class="info-box-text">Tổng</span>
-                                <span class="info-box-number">{!! $analytics['total'] !!}<small></small></span>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                        <!-- /.info-box -->
-                    </div>
-                    <div class="col-md-2 col-sm-6 col-xs-12">
-                        <div class="info-box">
-                            <span class="info-box-icon bg-aqua"><i class="ion ion-pricetag"></i></span>
+                            {{--<div class="info-box-content">--}}
+                                {{--<span class="info-box-text">Tổng</span>--}}
+                                {{--<span class="info-box-number">{!! $analytics['total'] !!}<small></small></span>--}}
+                            {{--</div>--}}
+                            {{--<!-- /.info-box-content -->--}}
+                        {{--</div>--}}
+                        {{--<!-- /.info-box -->--}}
+                    {{--</div>--}}
+                    {{--<div class="col-md-2 col-sm-6 col-xs-12">--}}
+                        {{--<div class="info-box">--}}
+                            {{--<span class="info-box-icon bg-aqua"><i class="ion ion-pricetag"></i></span>--}}
 
-                            <div class="info-box-content">
-                                <span class="info-box-text">Lợi Nhuận</span>
-                                <span class="info-box-number">{!! number_format($analytics['price']) !!}<small></small></span>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                        <!-- /.info-box -->
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-md-2 col-sm-6 col-xs-12">
-                        <div class="info-box">
-                            <span class="info-box-icon bg-red"><i class="ion ion-ios-cart-outline"></i></span>
+                            {{--<div class="info-box-content">--}}
+                                {{--<span class="info-box-text">Lợi Nhuận</span>--}}
+                                {{--<span class="info-box-number">{!! number_format($analytics['price']) !!}<small></small></span>--}}
+                            {{--</div>--}}
+                            {{--<!-- /.info-box-content -->--}}
+                        {{--</div>--}}
+                        {{--<!-- /.info-box -->--}}
+                    {{--</div>--}}
+                    {{--<!-- /.col -->--}}
+                    {{--<div class="col-md-2 col-sm-6 col-xs-12">--}}
+                        {{--<div class="info-box">--}}
+                            {{--<span class="info-box-icon bg-red"><i class="ion ion-ios-cart-outline"></i></span>--}}
 
-                            <div class="info-box-content">
-                                <span class="info-box-text">Thành công</span>
-                                <span class="info-box-number">{!! $analytics['success'] !!}</span>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                        <!-- /.info-box -->
-                    </div>
-                    <!-- /.col -->
+                            {{--<div class="info-box-content">--}}
+                                {{--<span class="info-box-text">Thành công</span>--}}
+                                {{--<span class="info-box-number">{!! $analytics['success'] !!}</span>--}}
+                            {{--</div>--}}
+                            {{--<!-- /.info-box-content -->--}}
+                        {{--</div>--}}
+                        {{--<!-- /.info-box -->--}}
+                    {{--</div>--}}
+                    {{--<!-- /.col -->--}}
 
-                    <!-- fix for small devices only -->
-                    <div class="clearfix visible-sm-block"></div>
+                    {{--<!-- fix for small devices only -->--}}
+                    {{--<div class="clearfix visible-sm-block"></div>--}}
 
-                    <div class="col-md-2 col-sm-6 col-xs-12">
-                        <div class="info-box">
-                            <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+                    {{--<div class="col-md-2 col-sm-6 col-xs-12">--}}
+                        {{--<div class="info-box">--}}
+                            {{--<span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>--}}
 
-                            <div class="info-box-content">
-                                <span class="info-box-text">Hủy</span>
-                                <span class="info-box-number">{!! $analytics['cancel'] !!}</span>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                        <!-- /.info-box -->
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-md-2 col-sm-6 col-xs-12">
-                        <div class="info-box">
-                            <span class="info-box-icon bg-yellow"><i class="ion ion-ios-cart-outline"></i></span>
+                            {{--<div class="info-box-content">--}}
+                                {{--<span class="info-box-text">Hủy</span>--}}
+                                {{--<span class="info-box-number">{!! $analytics['cancel'] !!}</span>--}}
+                            {{--</div>--}}
+                            {{--<!-- /.info-box-content -->--}}
+                        {{--</div>--}}
+                        {{--<!-- /.info-box -->--}}
+                    {{--</div>--}}
+                    {{--<!-- /.col -->--}}
+                    {{--<div class="col-md-2 col-sm-6 col-xs-12">--}}
+                        {{--<div class="info-box">--}}
+                            {{--<span class="info-box-icon bg-yellow"><i class="ion ion-ios-cart-outline"></i></span>--}}
 
-                            <div class="info-box-content">
-                                <span class="info-box-text">Trong ngày</span>
-                                <span class="info-box-number">{!! $analytics['today'] !!}</span>
-                            </div>
-                            <!-- /.info-box-content -->
-                        </div>
-                        <!-- /.info-box -->
-                    </div>
-                    <!-- /.col -->
-                </div>
-                <div class="row">
-                    @foreach($analytics['category'] as $category=>$count)
-                        <div class="col-lg-4 col-xs-6">
-                            <!-- small box -->
-                            <div class="small-box bg-aqua">
-                                <div class="inner">
-                                    <h3>{!! $count !!}</h3>
+                            {{--<div class="info-box-content">--}}
+                                {{--<span class="info-box-text">Trong ngày</span>--}}
+                                {{--<span class="info-box-number">{!! $analytics['today'] !!}</span>--}}
+                            {{--</div>--}}
+                            {{--<!-- /.info-box-content -->--}}
+                        {{--</div>--}}
+                        {{--<!-- /.info-box -->--}}
+                    {{--</div>--}}
+                    {{--<!-- /.col -->--}}
+                {{--</div>--}}
+                {{--<div class="row">--}}
+                    {{--@foreach($analytics['category'] as $category=>$count)--}}
+                        {{--<div class="col-lg-4 col-xs-6">--}}
+                            {{--<!-- small box -->--}}
+                            {{--<div class="small-box bg-aqua">--}}
+                                {{--<div class="inner">--}}
+                                    {{--<h3>{!! $count !!}</h3>--}}
 
-                                    <p>{!! $category !!}</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-bag"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
+                                    {{--<p>{!! $category !!}</p>--}}
+                                {{--</div>--}}
+                                {{--<div class="icon">--}}
+                                    {{--<i class="ion ion-bag"></i>--}}
+                                {{--</div>--}}
+                                {{--<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
 
-                    @endforeach
-                </div>
-            </div>
+                    {{--@endforeach--}}
+                {{--</div>--}}
+            {{--</div>--}}
 
             <div class="box-body">
 
@@ -173,22 +173,22 @@
     <script src="{{ asset('module/admin/assets/moment.min.js') }}"></script>
     <script src="{{ asset('module/admin/bower_components/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
     <script>
-        $('#reservation').daterangepicker({
-            //singleDatePicker: true,
-            //showDropdowns: true
-        }, function(start, end, label) {
-            $.ajax({
-                type: "GET",
-                data: {
-                   date_start:start.format('YYYY-MM-DD'),
-                   date_end:end.format('YYYY-MM-DD'),
-                },
-                success: function (data) {
-                   let content = $(data.views.content);
-                   let result_analytics = content.find('.result-analytics');
-                   $(".result-analytics").html(result_analytics.html());
-                },
-            });
-        });
+        // $('#reservation').daterangepicker({
+        //     //singleDatePicker: true,
+        //     //showDropdowns: true
+        // }, function(start, end, label) {
+        //     $.ajax({
+        //         type: "GET",
+        //         data: {
+        //            date_start:start.format('YYYY-MM-DD'),
+        //            date_end:end.format('YYYY-MM-DD'),
+        //         },
+        //         success: function (data) {
+        //            let content = $(data.views.content);
+        //            let result_analytics = content.find('.result-analytics');
+        //            $(".result-analytics").html(result_analytics.html());
+        //         },
+        //     });
+        // });
     </script>
 @endpush
