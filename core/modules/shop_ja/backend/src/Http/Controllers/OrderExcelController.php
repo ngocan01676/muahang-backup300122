311b13bffe133e0f64ddb271a0bc9d013a5714d2
SoftVN88 <softvn88@gmail.com>
2020-11-11 11:57:18 +0700

    a

--- core/modules/shop_ja/backend/src/Http/Controllers/OrderExcelController.php
+++ core/modules/shop_ja/backend/src/Http/Controllers/OrderExcelController.php
@@ -1051,7 +1051,7 @@ class OrderExcelController extends \Zoe\Http\ControllerBackend
                 },
                 "GetTimeCheck" => function ($model){
                     if($model->status > 0) {
-                        $diff = strtotime(date('Y-m-d H:i:s')) - strtotime($model->updated_at);
+                        $diff = strtotime("-30 minutes",time()) - strtotime($model->updated_at);
 
                         $years = floor($diff / (365 * 60 * 60 * 24));
                         $months = floor(($diff - $years * 365 * 60 * 60 * 24) / (30 * 60 * 60 * 24));
