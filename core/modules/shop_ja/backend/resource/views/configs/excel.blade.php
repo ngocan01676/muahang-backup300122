6af8df284c4632f642be7590f672b9e267668310
SoftVN88 <softvn88@gmail.com>
2020-11-16 17:08:59 +0700

    a

--- core/modules/shop_ja/backend/resource/views/configs/excel.blade.php
+++ core/modules/shop_ja/backend/resource/views/configs/excel.blade.php
@@ -1,5 +1,29 @@
 @php
     $category =  get_category_type("shop-ja:product:category");
+
+    $keys =  [
+            ['timeCreate',3.29,'ngày đặt hàng'],
+            ['payMethod',6.57,'Phương thức thanh toán'],//B
+            ['phone',10.86,'Số điện thoại'],//C
+            ['zipcode',6.57, 'Mã bưu điện'],//D
+            ['province',5.14,'Tỉnh/TP'],//E
+            ['address',28.71,'Địa chỉ giao hàng'],//F
+            ['fullname',14.71,'Họ tên người nhận'],//G
+            ['product_id',7,'Mã SP'],//H
+            ['product_name',18.57,'Tên SP'],//I
+            ['price',4.57, 'Giá nhập'],//J
+            ['count',2.86, 'SL'],//K
+            ['order_date',9,'Ngày nhận'],//L
+            ['order_hours',10, 'Giờ nhận'],//M
+            ['order_ship',4.71,'Phí ship'],//N
+            ['order_total_price',6.43, 'Tổng giá nhập'],//O
+            ['order_total_price_buy',8,'Total Bán'],//P
+            ['order_ship_cou',3.43,'Phí giao hàng'],//Q
+            ['order_price',5.43,'Lợi nhuận'],//R
+            ['order_tracking',4.86,'Mã tracking'],//S
+            ['order_info',8.57,'Thông tin chuyển khoản'],//T
+            ['order_link',25,'Đường dẫn'],//U
+        ];
 @endphp
 <table class="table table-bordered">
     <tbody>
@@ -16,7 +40,7 @@
                             @php $close = true; @endphp
                             <tr>
                                 @endif
-                                <td> <input type="checkbox" name="excel.{!! $value->name !!}.{!! $key !!}" value="1"> {!! z_language($val) !!}</td>
+                                <td> <input type="checkbox" name="excel.export.{!! $value->name !!}.{!! $val[0] !!}" value="1"> {!! z_language($val[2]) !!}</td>
                                 @if($i==5)
                                     @php $i=0; $close = false; @endphp
                             </tr>
