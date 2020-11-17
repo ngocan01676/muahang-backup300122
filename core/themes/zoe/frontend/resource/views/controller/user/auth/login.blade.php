29f0f3a19e935d67282cea24af16836069f51c7a
SoftVN88 <softvn88@gmail.com>
2020-11-17 14:37:36 +0700

    a

--- core/themes/zoe/frontend/resource/views/controller/user/auth/login.blade.php
+++ core/themes/zoe/frontend/resource/views/controller/user/auth/login.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h1>{!! z_language('Login') !!}</h1>
+                    <h1>{!! z_language('Login1') !!}</h1>
                 </div>
             </div>
         </div>
