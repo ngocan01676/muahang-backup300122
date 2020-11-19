c01cf6b3efb70785abc17f55bd8700cbf77184f6
SoftVN88 <softvn88@gmail.com>
2020-11-19 14:02:24 +0700

    a

--- core/modules/shop_ja/backend/src/Libs/Excel.php
+++ core/modules/shop_ja/backend/src/Libs/Excel.php
@@ -365,6 +365,7 @@ class Excel{
                 ),
             )
         );
+        $columns_value = array_flip($datas['columns']);
         $colums = [
             ["支払区分",'payMethod',10,9],//A Phương thức thanh toán
             ["到着希望日",'order_date1',15,9],//B ngày giao hàng
@@ -375,11 +376,11 @@ class Excel{
             ["配送先住所",['callback'=>function($index,$value){return preg_replace('/\s+/', '',$value );},'key'=>'address'],18,9], // G Địa chỉ giao hàng
             ["配送先電話番号",'phone1',10,9], // H Số điện thoại
             ["別途送料",'order_ship',15,9], //I Phí Ship
-            ["紹介料",['callback'=>function($index,$value){return (int)$value+330;},'key'=>'order_price'],15,9],// Lợi nhuận J
+            ["紹介料",,15,9],// Lợi nhuận J
             ["仕入金額",'order_total_price_buy',15,9], // Tổng giá đơn hàng K
             ["品番",['product'=>['product_id','code']],10,9], // Mã sản phẩm L
             ["商品名",['product'=>['product_id','title']],18,9], // Tên sản phẩm M
-            ["単価",'order_total_price',15,9], // Giá nhập N
+            ["単価",['callback'=>function($index,$value,$a,$values) use($columns_value){return (int)$value/(int)$values[$columns_value['count']];},'key'=>'order_total_price'],15,9], // Giá nhập N
             ["数量",'count',15,9], // Số lượng O
             ["",'',15,9], // Số lượng O
             ["",'order_info',15,9], // Số lượng O
@@ -425,7 +426,7 @@ class Excel{
         $orders = [
 
         ];
-        $columns_value = array_flip($datas['columns']);
+
         $products =  DB::table('shop_product')->get()->keyBy('id')->all();
         $images = [];
         $ids = [];
@@ -454,7 +455,7 @@ class Excel{
                         $sheet->setCellValue($nameCol.$start,$_val);
                     }else if(isset($value[1]['callback']) && isset($value[1]['key'])){
                         $conf = $value[1]['callback'];
-                        $_val = call_user_func_array($conf,[$start,(isset($columns_value[$value[1]['key']])?$values[$columns_value[$value[1]['key']]]:""),$nameCol.$start]);
+                        $_val = call_user_func_array($conf,[$start,(isset($columns_value[$value[1]['key']])?$values[$columns_value[$value[1]['key']]]:""),$nameCol.$start,$values]);
                         $sheet->setCellValue($nameCol.$start,$_val);
                     }
                 }else{
