49a1cfa96fe150054e6ab5a93dc697e8e4461edf
SoftVN88 <softvn88@gmail.com>
2020-11-19 15:30:58 +0700

    a

--- core/modules/shop_ja/backend/resource/views/form/order-excel.blade.php
+++ core/modules/shop_ja/backend/resource/views/form/order-excel.blade.php
@@ -135,9 +135,9 @@
         let stringDate = '{!! date('Y-m-d',strtotime($model?$model->key_date:time())) !!}';
         let  date = moment(stringDate);
 
-        // console.log = function () {
-        //
-        // };
+        console.log = function () {
+
+        };
 
     </script>
 
