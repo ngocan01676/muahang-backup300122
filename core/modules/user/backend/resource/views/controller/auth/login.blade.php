05c8dbb6320cdaccda25abfc8e1e2a24f9aec7b8
SoftVN88 <softvn88@gmail.com>
2020-11-23 21:11:47 +0700

    a

--- core/modules/user/backend/resource/views/controller/auth/login.blade.php
+++ core/modules/user/backend/resource/views/controller/auth/login.blade.php
@@ -32,7 +32,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="../../index2.html"><b>Zoe</b>CMS</a>
+        <img src="{!! url('/logo.jpg') !!}" alt="" style="width: 150px">
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
