66b5f93f7c703f7b55149825b5db39bd7bf40bdb
SoftVN88 <softvn88@gmail.com>
2020-11-26 09:53:52 +0700

    a

--- core/modules/shop_ja/backend/resource/views/form/order-excel.blade.php
+++ core/modules/shop_ja/backend/resource/views/form/order-excel.blade.php
@@ -217,7 +217,7 @@
             color: #000000;
         }
         .jexcel tbody tr.footer {
-            /*background-color: #FFFF00 !important;*/
+            background-color: #001bff !important;
         }
         .jexcel tbody tr.group-cell td{
 
