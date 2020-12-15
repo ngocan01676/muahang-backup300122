baa67cd1ae2d1ff0ee9a40e84fac04c9b0ca8874
SoftVN88 <softvn88@gmail.com>
2020-12-15 22:56:11 +0700

    a

--- lib/zoe/Console/Kernel.php
+++ lib/zoe/Console/Kernel.php
@@ -1,8 +1,9 @@
 <?php
-
 namespace Zoe\Console;
 use App\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
-
+    protected $commands = [
+        \Zoe\Console\Commands\MakePlugin::class
+    ];
 }
