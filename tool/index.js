1571d26bebdc1e3cf02e85fd30dd1061b4f11c92
SoftVN88 <softvn88@gmail.com>
2020-12-29 16:56:00 +0700

    a

--- tool/index.js
+++ tool/index.js
@@ -248,8 +248,8 @@ async function JAPAN_POST(tracking){
 
             let timeEnd = moment().add('-'+(60*24*0.5),'minutes').format("YYYY-MM-DD HH:mm:ss");
 
-            var sql = "SELECT * FROM `cms_shop_order_excel_tracking` where status != 1 and status < 10 and count<10 and (updated_at <= '"+timeEnd+"' or status=0) order by updated_at LIMIT 0,20";
-
+            // var sql = "SELECT * FROM `cms_shop_order_excel_tracking` where status != 1 and status < 10 and count<10 and (updated_at <= '"+timeEnd+"' or status=0) order by updated_at LIMIT 0,20";
+            var sql = "SELECT * FROM `cms_shop_order_excel_tracking` where status != 1 and status<10 and (updated_at <= '"+timeEnd+"' or status = 2 and updated_at = '"+moment().format("YYYY-MM-DD")+"') order by updated_at LIMIT 0,20";
             console.log(sql);
 
             let rows = {};
