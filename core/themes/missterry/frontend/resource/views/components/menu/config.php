1390c7b2d8ec5e28f368f254cf913656d90dd7ff
SoftVN88 <softvn88@gmail.com>
2021-01-08 01:21:25 +0700

    a

--- core/themes/missterry/frontend/resource/views/components/menu/config.php
+++ core/themes/missterry/frontend/resource/views/components/menu/config.php
@@ -13,6 +13,7 @@ return component_config($_opt_,
     component_config_views([
         'main' => ["label" => "Main", "view" => "main"],
         'footer' => ["label" => "Footer", "view" => "footer"],
+        'main-menu' => ["label" => "main-menu", "view" => "main-menu"],
     ]),
     ["name" => "Menu","view"=>""]
 );
