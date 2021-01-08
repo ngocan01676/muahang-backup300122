b4dfb607b7ac6e25e78ef5c6ba84ef397b698bd1
SoftVN88 <softvn88@gmail.com>
2021-01-08 22:21:20 +0700

    a

--- tool/index.js
+++ tool/index.js
@@ -388,6 +388,7 @@ async function JAPAN_POST(tracking){
         AddQueue('setInterval');
     },60000);
     setInterval(function () {
+        console.log('lock:'+lock);
         if(lock === false ){
             if(pushData.length > 0){
                 lock = true;
