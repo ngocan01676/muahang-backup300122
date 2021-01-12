eb81f86eb6d08ffe596b5888ade4b3ab5a61df1c
SoftVN88 <softvn88@gmail.com>
2021-01-12 17:39:08 +0700

    a

--- core/modules/Admin/backend/resource/views/form/email-template.blade.php
+++ core/modules/Admin/backend/resource/views/form/email-template.blade.php
@@ -72,7 +72,7 @@
                                         table  : {title : 'Table' , items : 'inserttable tableprops deletetable | cell row column'},
                                         tools  : {title : 'Tools' , items : 'spellchecker code'},
                                     },
-                                    menubar: 'file edit insert view format table tools',
+                                    menubar: 'file edit insert view format table tools ',
                                     setup: function(editor) {
                                             @verbatim
                                             editor.on('change', function(e) {
