7b0c9b4d6c512e85cfecd1b87c8c5d8dffb901f1
SoftVN88 <softvn88@gmail.com>
2021-01-12 21:38:29 +0700

    a

--- core/plugins/Comment/resource/configs/sidebar.php
+++ core/plugins/Comment/resource/configs/sidebar.php
@@ -2,14 +2,14 @@
 
 return [
     "sidebars" => [
-        "plugin:item" => [
-            "items" => [
-                "comment" => [
-                    "name" => z_language("Comment"),
-                    "url" => "backend:plugin:comment:list",
-                    "icon" =>"fa fa-comments"
-                ],
-            ]
-        ]
+//        "plugin:item" => [
+//            "items" => [
+//                "comment" => [
+//                    "name" => z_language("Comment"),
+//                    "url" => "backend:plugin:comment:list",
+//                    "icon" =>"fa fa-comments"
+//                ],
+//            ]
+//        ]
     ]
 ];
