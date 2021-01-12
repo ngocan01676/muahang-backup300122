887f1f3322c388f2f056abff807d63c4153337da
SoftVN88 <softvn88@gmail.com>
2021-01-12 17:40:00 +0700

    a

--- core/modules/Admin/backend/resource/views/form/email-template.blade.php
+++ core/modules/Admin/backend/resource/views/form/email-template.blade.php
@@ -64,13 +64,7 @@
                                     selector: "textarea.my-editor",
                                     height : "480",
                                     menu : {
-                                        file   : {title : 'File'  , items : 'newdocument'},
-                                        edit   : {title : 'Edit'  , items : 'undo redo | cut copy paste pastetext | selectall'},
-                                        insert : {title : 'Insert', items : 'link media | template hr'},
-                                        view   : {title : 'View'  , items : 'visualaid'},
-                                        format : {title : 'Format', items : 'bold italic underline strikethrough superscript subscript | formats | removeformat'},
-                                        table  : {title : 'Table' , items : 'inserttable tableprops deletetable | cell row column'},
-                                        tools  : {title : 'Tools' , items : 'spellchecker code'},
+                                        
                                     },
                                     menubar: 'file edit insert view format table tools ',
                                     setup: function(editor) {
