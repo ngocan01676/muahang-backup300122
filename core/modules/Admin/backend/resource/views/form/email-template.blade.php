f6e9f1d6dd811cc7464a765c7e2c528ee425bf91
SoftVN88 <softvn88@gmail.com>
2021-01-12 17:44:38 +0700

    a

--- core/modules/Admin/backend/resource/views/form/email-template.blade.php
+++ core/modules/Admin/backend/resource/views/form/email-template.blade.php
@@ -115,7 +115,19 @@
                                             }
                                             echo 'console.log(config);';
                                 @endphp
-                                tinymce.init(config);
+                               // tinymce.init(config);
+                                tinymce.init({
+                                    selector: "textarea.my-editor",  // change this value according to your HTML
+                                    menu: {
+                                        file: {title: 'File', items: 'newdocument'},
+                                        edit: {title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall'},
+                                        insert: {title: 'Insert', items: 'link media | template hr'},
+                                        view: {title: 'View', items: 'visualaid'},
+                                        format: {title: 'Format', items: 'bold italic underline strikethrough superscript subscript | formats | removeformat'},
+                                        table: {title: 'Table', items: 'inserttable tableprops deletetable | cell row column'},
+                                        tools: {title: 'Tools', items: 'spellchecker code'}
+                                    }
+                                });
                             </script>
                             @endsection
                         </td>
