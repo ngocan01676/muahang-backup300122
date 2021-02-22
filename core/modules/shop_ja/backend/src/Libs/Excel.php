93a1eb0eaa24143188ec87666dfae998d7e06bf2
SoftVN88 <softvn88@gmail.com>
2021-02-22 11:03:55 +0700

    a

--- core/modules/shop_ja/backend/src/Libs/Excel.php
+++ core/modules/shop_ja/backend/src/Libs/Excel.php
@@ -2090,7 +2090,7 @@ class Excel
                         } catch (\Exception $ex) {
                             $array_count = [];
                         }
-                        $product_title = $count;
+                        $product_title = "";
 
                         foreach ($array_product as $pro_id) {
                             if (isset($products[$pro_id]) && isset($array_count[$pro_id]) && $array_count[$pro_id] > 0) {
@@ -2109,7 +2109,7 @@ class Excel
                                 }
                             }
                         }
-                        var_dump($product_title);
+                        
                         return rtrim($product_title, '、');
                     }, 'key' => 'product_name'], 18, 9],//I
                 ["単価", 'price', 15, 9],//Giá nhập
