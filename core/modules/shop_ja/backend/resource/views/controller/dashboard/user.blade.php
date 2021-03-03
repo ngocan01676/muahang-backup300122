3accd073d42ab102b9f5c01a1ca03233c4684023
SoftVN88 <softvn88@gmail.com>
2021-03-03 09:39:01 +0700

    a

--- core/modules/shop_ja/backend/resource/views/controller/dashboard/user.blade.php
+++ core/modules/shop_ja/backend/resource/views/controller/dashboard/user.blade.php
@@ -546,7 +546,7 @@
          //   $datepicker_start.datepicker('setDate',Date.parse('{!! date("Y-m-d", strtotime("first day of this month")) !!}'));
             var myDate = new Date({!! date("Y", strtotime("last day of this month")) !!},{!! (int)date("m", strtotime("last day of this month")) !!}-1,{!! date("d", strtotime("last day of this month")) !!});
             console.log(myDate.toISOString().substr(0, 10));
-            var myDate1 = new Date({!! date("Y", strtotime("first day of this month")) !!},{!! (int)date("m", strtotime("first day of this month")) !!}-1,{!! date("d", strtotime("first day of this month")) !!});
+            var myDate1 = new Date({!! date("Y", strtotime("first day of this month")) !!},{!! (int)date("m", strtotime("first day of this month")) !!},{!! date("d", strtotime("first day of this month")) !!});
             console.log(myDate1.toISOString().substr(0, 10));
             var $datepicker_end = $('#datepicker_end').datepicker({
                 autoclose: true,
