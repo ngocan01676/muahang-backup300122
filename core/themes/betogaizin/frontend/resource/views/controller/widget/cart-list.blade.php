76484c97b2fb8ad64813920e001b83642069194f
SoftVN88 <softvn88@gmail.com>
2021-04-05 06:56:15 +0700

    aaaaa

---
+++ core/themes/betogaizin/frontend/resource/views/controller/widget/cart-list.blade.php
@@ -0,0 +1,34 @@
+@section('content')
+    <div class="minicart-dropdown-trigger-inner">
+        <div class="header-utility-cart-grid">
+            <div class="header-utility-cart-grid-item">
+                <p class="header-utility-cart-icon">
+                    <span class="svg-icon icon-28 icon-cart-black">
+                        <span data-auto-id="cart-indicator" class="popout">1</span>
+                    </span>
+                </p>
+            </div>
+            <div class="header-utility-cart-grid-item">
+                <div class="header-utility-cart-txt">
+                    <p class="header-utility-cart-txt-price">
+                        1,998
+                        <span class="unit">円(税込)</span>
+                    </p>
+                    <p class="header-utility-cart-txt-fee">
+                        送料 330<span class="unit">円(税込)</span></p>
+                    <p class="header-utility-cart-txt-free">
+                        あと3502
+                        <span class="unit">円(税込)</span>で送料無料
+                    </p>
+                </div>
+            </div>
+            <div class="header-utility-cart-grid-item">
+                <p class="header-utility-cart-btn">
+                    <a data-auto-id="go-step-pc" href="javascript:void(0);" data-ratid="cart_button_click" data-ratevent="click" data-ratparam="all">
+                        レジに進む
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+@endsection
