4bf8c166bb8084e1382e9587825abb0ad8358081
SoftVN88 <softvn88@gmail.com>
2021-06-01 12:34:37 +0700

    aaa

--- core/modules/BetoGaizin/backend/resource/views/composer/View-Company-Prototype.blade.php
+++ core/modules/BetoGaizin/backend/resource/views/composer/View-Company-Prototype.blade.php
@@ -2,7 +2,7 @@
   $categorys = config_get("category", "shop-ja:product:category");
 @endphp
 <td style="width: 20%">
-    <select onchange="GetProduct(this,null)" class="form-control data"  data-rename="{!! isset($columns['rename'])?$columns['rename']:false !!}"
+    <select onchange="GetProduct(this,'')" class="form-control data"  data-rename="{!! isset($columns['rename'])?$columns['rename']:false !!}"
             data-key="{!! $columns['name'] !!}"
             data-name="{!! $DataComposer['config']['name'] !!}[@INDEX@].{!! $columns['name'] !!}">
         <option value="0">Chọn</option>
