6dcc818de9626ae4d5f25549467938105c49e5a4
SoftVN88 <softvn88@gmail.com>
2021-06-11 00:28:45 +0700

    aaaa

--- core/themes/betogaizin/frontend/src/Http/Controllers/HomeController.php
+++ core/themes/betogaizin/frontend/src/Http/Controllers/HomeController.php
@@ -33,7 +33,7 @@ class HomeController extends \Zoe\Http\ControllerFront
         $cate = $cate[$id];
 
         DB::connection()->enableQueryLog();
-        $model = DB::table('shop_product as p')->where('p.status',1)->where('p.group_id',$id)
+        $model = DB::table('shop_product as p')->where('p.status',1)
             ->join('shop_product_translation as t','t._id','=','p.id')
             ->select('p.id','p.image','p.price_buy','p.category_id','t.name','t.slug as slug','t.content')
             ->where('lang_code',$config_language['lang']);
