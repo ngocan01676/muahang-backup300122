b42b079171f48543e0f89b0b889a64289dc2a99c
SoftVN88 <softvn88@gmail.com>
2021-06-11 15:34:45 +0700

    aaa

--- core/themes/betogaizin/frontend/resource/views/widgets/Footer-Info/views/main.blade.php
+++ core/themes/betogaizin/frontend/resource/views/widgets/Footer-Info/views/main.blade.php
@@ -1,7 +1,71 @@
-<div class="box box-primary mt20">
-    <ul class="list-disc">
-        <li>法令により20歳未満への酒類販売はいたしません。20歳未満の飲酒は法律で禁止されています。</li>
-        <li>写真はイメージです。実際にお届けする商品とパッケージなどが異なる場合がございます。商品名・規格などは予告なく変更になる場合がございます。</li>
-        <li>天候不順および市場等の事情により、商品をお届けできない場合や、産地が変更になる場合がございます。予めご了承ください。</li>
-    </ul>
+<div class="pc-site-footer mt0">
+    <div class="section block01">
+        <div class="inner">
+            <div class="flex-container with-margin">
+                <div class="row">
+                    <div class="col left-col">
+                        <h2 class="title-large">
+                            <span class="svg-icon icon-40 icon-help"><a href="#">{!! z_language('Hỗ trợ') !!}</a></span>
+                        </h2>
+                        <div class="flex-container with-margin border-right">
+                            <div class="row">
+                                <div class="col wid4">
+                                    <ul class="guide-link">
+                                        <li><a href="#" class="">はじめての方へ</a></li>
+                                        <li><a href="#">会員登録方法</a></li>
+                                        <li><a href="#">配送料・利用料</a></li>
+                                        <li><a href="#">配送時間</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col wid4">
+                                    <ul class="guide-link">
+                                        <li><a href="#">お支払い方法</a></li>
+                                        <li><a href="#">クーポンの使い方</a></li>
+                                        <li><a href="#">ご注文のキャンセル</a></li>
+                                        <li><a href="#" class="">再配送手続き</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col wid4">
+                                    <ul class="guide-link">
+                                        <li><a href="#">配送エリア一覧</a></li>
+                                        <li><a href="#" to="/info/liquor?l-id=_footer_shuruikanri" class="">酒類販売管理者標識</a></li>
+                                        <li><a href="#">お問い合わせ</a></li>
+                                        <li><a href="#">法人様のご利用について</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col right-col">
+                        <h2 class="title-large"><span>{!! z_language('Các ý kiến') !!}</span></h2>
+                        <form>
+                            <div class="form-textarea">
+                                <textarea name="message"></textarea>
+                            </div>
+                            <div class="flex-container with-margin">
+                                <p class="form-note mb0">
+                                    {!! z_language('Chúng tôi không trả lời riêng lẻ cho ý kiến ​​này. Các mục yêu cầu trả lời / phản hồi, chẳng hạn như thay đổi và hủy đơn đặt hàng') !!}
+                                    <a href="#" class="link-color-red">{!! z_language('Liên hệ chúng tôi') !!}</a>
+                                   {!! z_language('Vui lòng liên hệ với chúng tôi từ') !!}
+                                </p>
+                                <div class="row">
+                                    <p class="advise-box">{!! z_language('Chúng tôi đang cải thiện trang web dựa trên ý kiến ​​của khách hàng. Chi tiết cải tiến') !!}
+                                        <a href="/info/voice?l-id=footer_info_message" to="/info/voice?l-id=footer_info_message" class="">こちら</a>
+                                    </p>
+                                    <div class="col wid5 form-right">
+                                        <p class="submit-btn disabled">
+                                            <a><span>{!! z_language('Gửi một ý kiến') !!}</span></a>
+                                        </p>
+                                    </div>
+                                    <div class="col wid5 form-right-col2"></div>
+                                    <div class="col wid5"></div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
