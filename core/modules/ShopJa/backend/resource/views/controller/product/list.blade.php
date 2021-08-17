524ab4cb95536727e4e5183e189cad327b3267ad
SoftVN88 <softvn88@gmail.com>
2021-08-17 13:17:58 +0700

    aaaa

--- core/modules/ShopJa/backend/resource/views/controller/product/list.blade.php
+++ core/modules/ShopJa/backend/resource/views/controller/product/list.blade.php
@@ -146,8 +146,8 @@
                         type:$("#myform input[type='radio']:checked").val()
                     },
                     success:function (data) {
-
-                        window.location.href = window.location.protocol+"//"+getDomain(location.href)+"/"+data.url;
+                        window.open(window.location.protocol+"//"+getDomain(location.href)+"/"+data.url, '_blank').focus();
+                       
                     }
                 });
             });
