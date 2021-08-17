bdb24d3ea0b9ba7cfef458ad68543b65f5c9a370
SoftVN88 <softvn88@gmail.com>
2021-08-17 17:22:57 +0700

    123

--- core/modules/ShopJa/backend/src/Http/Controllers/ProductController.php
+++ core/modules/ShopJa/backend/src/Http/Controllers/ProductController.php
@@ -375,12 +375,12 @@ class ProductController extends \Zoe\Http\ControllerBackend
             ->join('shop_product_translation as t','t._id','=','p.id')
             ->select('p.id','p.image','p.price_buy','p.category_id','t.name','t.slug as slug','t.content','p.price_buy_km')
             ->whereIn('p.category_id',$cates)
-            ->where('lang_code','vi');
+            ->where('lang_code','vi')->get()->all();
         $xml = '<?xml version="1.0" encoding="UTF-8"?>';
         $xml.= '<feed xmlns="http://www.w3.org/2005/Atom" xmlns:g="http://base.google.com/ns/1.0">';
         $xml.='<title>Betogaizin</title>';
         $xml.='<link href="https://muahang.tokyo" rel="self"/>';
-        dd($shop_product);
+       
         foreach ($shop_product as $k=>$v){
             $xml.='<entry>';
                 $xml.='<g:id>'.$v->id.'</g:id>';
