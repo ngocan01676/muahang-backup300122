48a6e74f38284b4ef50613650916f9ef02cefdfb
SoftVN88 <softvn88@gmail.com>
2021-08-31 14:40:48 +0700

    a

--- core/themes/betogaizin/frontend/resource/views/widgets/Home-Category-Product/main.php
+++ core/themes/betogaizin/frontend/resource/views/widgets/Home-Category-Product/main.php
@@ -14,7 +14,7 @@ function Main($option){
     if($category){
 
         $config_language = app()->config_language;
-       
+
         if(isset($config_language['lang'])){
 
             $cate =(array) DB::table('categories_translation')
