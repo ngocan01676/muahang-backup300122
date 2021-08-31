aa5f230bb4677393424a983f164d833c15d180a9
SoftVN88 <softvn88@gmail.com>
2021-08-31 15:45:25 +0700

    aa

--- core/themes/betogaizin/frontend/resource/views/widgets/Home-Banner/views/main.blade.php
+++ core/themes/betogaizin/frontend/resource/views/widgets/Home-Banner/views/main.blade.php
@@ -13,28 +13,28 @@
                 <div class="feature-items" data-v-12143966="">
                     <div class="feature-items-inner" data-v-12143966="">
                         <div class="feature-item" data-v-12143966="">
-                            <div data-v-12143966=""><img src="/theme/betogaizin/icons/181761a593d4658a3cc5.jpg" alt="西友の「毎日低価格」をネットでも" width="72" height="72" data-v-12143966=""></div>
+                            <div data-v-12143966=""><img src="/theme/betogaizin/icons/sale.png" alt="西友の「毎日低価格」をネットでも" width="72" height="72" data-v-12143966=""></div>
                             <div class="feature-item-text" data-v-12143966="">
                                 <div class="text-detail" data-v-12143966="">{!! z_language('西友の「毎日低価格」をネットでも') !!}</div>
                                 <div class="text-detail" data-v-12143966=""></div>
                             </div>
                         </div>
                         <div class="feature-item" data-v-12143966="">
-                            <div data-v-12143966=""><img src="/theme/betogaizin/icons/e76296d064a192ffcbb0.jpg" alt="最短当日配送でお届け" width="72" height="72" data-v-12143966=""></div>
+                            <div data-v-12143966=""><img src="/theme/betogaizin/icons/ex.png" alt="最短当日配送でお届け" width="72" height="72" data-v-12143966=""></div>
                             <div class="feature-item-text" data-v-12143966="">
                                 <div class="text-detail" data-v-12143966="">{!! z_language('最短当日配送でお届け') !!}</div>
                                 <div class="text-detail" data-v-12143966=""></div>
                             </div>
                         </div>
                         <div class="feature-item" data-v-12143966="">
-                            <div data-v-12143966=""><img src="/theme/betogaizin/icons/67276b9399e26fbc36f3.jpg" alt="入会費無料,一定額以上で送料無料" width="72" height="72" data-v-12143966=""></div>
+                            <div data-v-12143966=""><img src="/theme/betogaizin/icons/fre.png" alt="入会費無料,一定額以上で送料無料" width="72" height="72" data-v-12143966=""></div>
                             <div class="feature-item-text" data-v-12143966="">
                                 <div class="text-detail" data-v-12143966="">{!! z_language('入会費無料') !!}</div>
                                 <div class="text-detail" data-v-12143966="">{!! z_language('一定額以上で送料無料') !!}</div>
                             </div>
                         </div>
                         <div class="feature-item" data-v-12143966="">
-                            <div data-v-12143966=""><img src="/theme/betogaizin/icons/dbda3d6fcf1e3940600f.jpg" alt="{!! z_language('楽天ポイント,貯まる/使える') !!}" width="72" height="72" data-v-12143966=""></div>
+                            <div data-v-12143966=""><img src="/theme/betogaizin/icons/save-money.png" alt="{!! z_language('楽天ポイント,貯まる/使える') !!}" width="72" height="72" data-v-12143966=""></div>
                             <div class="feature-item-text" data-v-12143966="">
                                 <div class="text-detail" data-v-12143966="">{!! z_language('楽天ポイント') !!}</div>
                                 <div class="text-detail" data-v-12143966="">{!! z_language('貯まる/使える') !!}</div>
